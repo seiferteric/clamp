2a19126e6b72e9ff81705f0c7966e8b72f63a6b0
Eric Seifert <eric.seifert@cyaninc.com>
2015-05-29 17:06:53 -0700

    Get setup.py version from changelog

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
+import subprocess
+ver = subprocess.check_output(["dpkg-parsechangelog", "-SVersion"]).strip().decode("utf-8", "strict")
 
 setup(
       name = "clamp",
-      version = '0.0.1',
-      #packages = find_packages(),
+      version = ver,
       author = "Eric Seifert",
       author_email = "seiferteric@gmail.com",
       description = "CLAMP CLI alias tool with variable substitution.",
